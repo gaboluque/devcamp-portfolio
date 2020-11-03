35556455175b37480451ad6978bd4a53fa687a8f
Gabriel Luque <gluque>
2020-11-03 14:16:04 -0500

    Edit and update functinality on portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -22,6 +22,22 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Porfolio updated.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
   private
 
   def portfolio_params
