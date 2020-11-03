2b1cf6688d29d8501a81eb62ec0dfa011ea97935
Gabriel Luque <gluque>
2020-11-03 12:54:39 -0500

    New and create portfolio functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,4 +5,32 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Porfolio created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params
+      .require(:portfolio)
+      .permit(:title, :subtitle, :body)
+      .merge(
+        main_image: 'https://picsum.photos/600/400',
+        thumb_image: 'https://picsum.photos/350/200'
+      )
+  end
 end
