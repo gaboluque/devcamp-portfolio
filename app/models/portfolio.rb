8777e3448741fa14c6db7c9e404279ca30e34cda
Gabriel Luque <gluque>
2020-11-10 09:35:39 -0500

    Integrate nested attributes for technologies

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,15 +6,17 @@ class Portfolio < ApplicationRecord
 
   has_many :technologies
 
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |tech| tech['name'].blank? }
+
   validates_presence_of :title, :body, :main_image, :thumb_image
+  after_initialize :set_defaults
 
   def self.react
     where(subtitle: 'ReactJS')
   end
 
   scope :rails, -> { where(subtitle: 'Ruby on Rails') }
-
-  after_initialize :set_defaults
 
   def set_defaults
     self.main_image ||= Placeholder.image_generator(width: 600, height: 400)
