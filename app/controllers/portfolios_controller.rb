ee36c57fa741c2d057c03dd5d6a4129e41f40865
Gabriel Luque <gluque>
2020-11-10 10:05:42 -0500

    Nested attribute form for technologies

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
@@ -55,6 +56,6 @@ class PortfoliosController < ApplicationController
   def portfolio_params
     params
       .require(:portfolio)
-      .permit(:title, :subtitle, :body)
+      .permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,7 +7,7 @@ class Portfolio < ApplicationRecord
   has_many :technologies
 
   accepts_nested_attributes_for :technologies,
-                                reject_if: lambda { |tech| tech['name'].blank? }
+                                reject_if: ->(tech) { tech['name'].blank? }
 
   validates_presence_of :title, :body, :main_image, :thumb_image
   after_initialize :set_defaults
