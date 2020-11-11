c015d0f3efea91f61d4059211ce38af6a96563f5
Gabriel Luque <gluque>
2020-11-11 11:16:54 -0500

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
+# frozen_string_literal: true
+
+# User Model
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  validates_presence_of :name
+
+  def first_name
+    name.split.first
+  end
+
+  def last_name
+    name.split.last
+  end
 end
