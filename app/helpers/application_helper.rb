a13ac9a68b7bbde9864bf163e945b912536d65f4
Gabriel Luque <gluque>
2020-12-01 16:48:05 -0500

    Extract nav logic to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 # Application Helper
 module ApplicationHelper
-  def login_helper(style = "")
+  def login_helper(style = '')
     if current_user.is_a?(GuestUser)
       (link_to 'Register', new_user_registration_path, class: style) +
         ' '.html_safe +
@@ -16,5 +14,34 @@ module ApplicationHelper
     return unless session[:source]
 
     content_tag(:p, "Thanks for visiting me from #{session[:source]}")
+  end
+
+  def nav_helper(tag = 'span', style = '')
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag}>
+                      <a href=\"#{item[:path]}\" 
+                         class=\"#{style} #{active?(item[:path])}\">
+                          #{item[:title]}
+                      </a>
+                    </#{tag}>"
+    end
+    nav_links.html_safe
+  end
+
+  private
+
+  def nav_items
+    [
+      { path: root_path, title: 'Home' },
+      { path: about_path, title: 'About' },
+      { path: contact_path, title: 'Contact' },
+      { path: blogs_path, title: 'Blog' },
+      { path: portfolios_path, title: 'Portfolios' }
+    ]
+  end
+
+  def active?(path)
+    'active' if current_page? path
   end
 end
